37b301507d59c5ef3db5f7ae488c1fb81c0a6a0d
demina-ivan23 <demina.ivann@gmail.com>
2024-02-11 07:25:53 +0200

    homepage layout sketch commit (to be continued)

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,12 +5,34 @@
 
 <div class="card mt-3">
     <div class="card-body">
-        <div class="d-flex">
-            
+        <div class="header">
+            <div class="header__slideshow">
+                <h1>This Is A Home Page</h1>
+            </div>
+            <div class="header__stats">
+                <ul>
+                    <li>
+                        <h5>{{auth()->user()->name}}</h5>
+                    </li>
+                    <li>
+                        <h5>{{auth()->user()->email}}</h5>
+                    </li>
+                    <li>
+                        <h5>This Is A Stat Element</h5>
+                    </li>
+                    
+                </ul>
+            </div>
+            <div class="header__text">
+                
+            </div>
         </div>
     </div>
 </div>
 
 </div>
 
+@endsection
+@section('styles')
+    <link href="{{ asset('sass/home.css') }}" rel="stylesheet">
 @endsection
